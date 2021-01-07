869c1ad57688447c51c147bb3bda59cf9a5ff25a
Jonathan Jogenfors <etnoy@broach.se>
2021-01-07 22:42:57 +0100

    Documenting the sql injection tutorial

--- server/src/main/java/org/owasp/herder/module/sqlinjection/SqlInjectionTutorial.java
+++ server/src/main/java/org/owasp/herder/module/sqlinjection/SqlInjectionTutorial.java
@@ -57,49 +57,66 @@ public class SqlInjectionTutorial extends BaseModule {
     this.keyService = keyService;
   }
 
-  private Mono<Void> populate(DatabaseClient databaseClient, final long userId) {
+  /**
+   * Execute a given SQL injection query
+   *
+   * @param userId the currently logged in user id
+   * @param injectionQuery the query to be executed
+   * @return the result of the query
+   */
+  public Flux<SqlInjectionTutorialRow> submitQuery(final long userId, final String injectionQuery) {
+
+    // Each module and user has a unique in-memory database
+    final String dbName = String.format("%s-uid-%d", MODULE_NAME, userId);
+
+    final DatabaseClient databaseClient = sqlInjectionDatabaseClientFactory.create(dbName);
+
+    // A randomly chosen "username" that contains the flag
     final String hiddenName =
         Base64.getEncoder().encodeToString(keyService.generateRandomBytes(16));
 
+    // Create the SQL query used to populate the database
     final Mono<String> populationQuery =
+        // Compute the flag to be hidden in the database
         getFlag(userId)
             .map(
                 flag ->
                     String.format(
-                        "DROP ALL OBJECTS;"
+                        "DROP ALL OBJECTS;" // Clear the database
                             + "CREATE SCHEMA sqlinjection;"
+                            // Hidden credits are always fun
                             + "CREATE TABLE sqlinjection.users (name VARCHAR(255) PRIMARY KEY, comment VARCHAR(255));"
                             + "INSERT INTO sqlinjection.users values ('Jonathan Jogenfors', 'System Author');"
                             + "INSERT INTO sqlinjection.users values ('Niklas Johansson', 'Teacher');"
                             + "INSERT INTO sqlinjection.users values ('Jan-Åke Larsson', 'Professor');"
                             + "INSERT INTO sqlinjection.users values ('Guilherme B. Xavier','Examiner');"
-                            + "INSERT INTO sqlinjection.users values ('OR 1=1', 'You are close! Surround the query with single quotes so that your code is interpreted');"
+                            // This is the row that contains the flag!
                             + "INSERT INTO sqlinjection.users values ('%s', 'Well done, flag is %s');",
                         hiddenName, flag));
 
-    return populationQuery.flatMap(query -> databaseClient.execute(query).then());
-  }
+    // The query to be executed on the database. Note: if a vulnerability scanner has
+    // issues with this line, please ignore. This is *supposed* to be vulnerable, it's a CTF after
+    // all!
+    final String vulnerableQuery =
+        String.format("SELECT * FROM sqlinjection.users WHERE name = '%s'", injectionQuery);
 
-  public Flux<SqlInjectionTutorialRow> submitQuery(final long userId, final String usernameQuery) {
-    final String dbName = String.format("%s-uid-%d", MODULE_NAME, userId);
-
-    final DatabaseClient databaseClient = sqlInjectionDatabaseClientFactory.create(dbName);
-
-    // Create the database query. Yes, this is vulnerable to SQL injection. That's
-    // the whole point.
-    final String injectionQuery =
-        String.format("SELECT * FROM sqlinjection.users WHERE name = '%s'", usernameQuery);
-
-    return populate(databaseClient, userId)
-        // Execute database query
+    return
+    // Take the SQL query for initial population
+    populationQuery
+        // Execute it on the in-memory database
+        .flatMap(query -> databaseClient.execute(query).then())
+        // Then execute the SQL injection and fetch the results
         .thenMany(
-            databaseClient.execute(injectionQuery).as(SqlInjectionTutorialRow.class).fetch().all())
+            databaseClient.execute(vulnerableQuery).as(SqlInjectionTutorialRow.class).fetch().all())
+        // Some errors are to be shown to the end user, while all the rest are handled as usual
         .onErrorResume(
             exception -> {
-              // We want to forward database syntax errors to the user
+              // We want to forward database syntax errors and integrity violation errors to the
+              // user
               if ((exception instanceof BadSqlGrammarException)
                   || (exception instanceof DataIntegrityViolationException)) {
                 return Flux.just(
+                    // Build a row with only the error field filled in
                     SqlInjectionTutorialRow.builder()
                         .error(exception.getCause().getCause().toString())
                         .build());
