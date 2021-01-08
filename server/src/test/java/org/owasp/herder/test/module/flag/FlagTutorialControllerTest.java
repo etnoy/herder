cae423eb43eab6afa783ce974c4f9cb7a4030283
Jonathan Jogenfors <etnoy@broach.se>
2021-01-08 23:57:15 +0100

    Full flag tutorial controller test coverage

---
+++ server/src/test/java/org/owasp/herder/test/module/flag/FlagTutorialControllerTest.java
@@ -0,0 +1,95 @@
+/* 
+ * Copyright 2018-2021 Jonathan Jogenfors, jonathan@jogenfors.se
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.owasp.herder.test.module.flag;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.owasp.herder.authentication.ControllerAuthentication;
+import org.owasp.herder.exception.NotAuthenticatedException;
+import org.owasp.herder.module.flag.FlagTutorial;
+import org.owasp.herder.module.flag.FlagTutorialController;
+import org.owasp.herder.module.flag.FlagTutorialResult;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("FlagController unit test")
+class FlagTutorialControllerTest {
+
+  @BeforeAll
+  private static void reactorVerbose() {
+    // Tell Reactor to print verbose error messages
+    Hooks.onOperatorDebug();
+  }
+
+  private FlagTutorialController flagTutorialController;
+
+  @Mock private FlagTutorial flagTutorial;
+
+  @Mock private ControllerAuthentication controllerAuthentication;
+
+  @BeforeEach
+  private void setUp() {
+    // Set up the system under test
+    flagTutorialController = new FlagTutorialController(flagTutorial, controllerAuthentication);
+  }
+
+  @Test
+  void getFlag_UserNotAuthenticated_ReturnsException() {
+    when(controllerAuthentication.getUserId())
+        .thenReturn(Mono.error(new NotAuthenticatedException()));
+
+    StepVerifier.create(flagTutorialController.getFlag())
+        .expectError(NotAuthenticatedException.class)
+        .verify();
+
+    verify(controllerAuthentication, times(1)).getUserId();
+  }
+
+  @Test
+  void submitFlag_UserAuthenticated_ReturnsFlag() {
+    final long mockUserId = 417L;
+    final String flag = "validflag";
+
+    when(controllerAuthentication.getUserId()).thenReturn(Mono.just(mockUserId));
+    when(flagTutorial.getFlag(mockUserId)).thenReturn(Mono.just(flag));
+
+    FlagTutorialResult mockFlag = FlagTutorialResult.builder().flag(flag).build();
+
+    StepVerifier.create(flagTutorialController.getFlag())
+        .expectNext(mockFlag)
+        .expectComplete()
+        .verify();
+
+    verify(controllerAuthentication, times(1)).getUserId();
+    verify(flagTutorial, times(1)).getFlag(mockUserId);
+  }
+}
