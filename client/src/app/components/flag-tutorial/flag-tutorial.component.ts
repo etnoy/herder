d95f5c1b76e59fcc66b23debe85ae032dc42e86b
Jonathan Jogenfors <etnoy@broach.se>
2022-01-13 23:51:38 +0100

    Enforce style in CI

--- client/src/app/components/flag-tutorial/flag-tutorial.component.ts
+++ client/src/app/components/flag-tutorial/flag-tutorial.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ApiService } from '../../service/api.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Module } from 'src/app/model/module';
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
 import { User } from 'src/app/model/user';
 
