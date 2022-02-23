12cdcc6d08a3d675b972717f70d94ceaa7cad65d
Jonathan Jogenfors <etnoy@broach.se>
2022-02-23 07:53:56 +0100

    Remove unused vars

--- client/src/app/service/api.service.spec.ts
+++ client/src/app/service/api.service.spec.ts
@@ -3,14 +3,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { ApiService } from './api.service';
 import { RouterTestingModule } from '@angular/router/testing';
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
-  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,7 +14,6 @@ describe('ApiService', () => {
       providers: [ApiService],
     });
     service = TestBed.inject(ApiService);
-    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
